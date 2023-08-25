29a63c730498c84a3a951464af476f5f1570e50f
Sanjay244-Rathore <sanjaykumar71244bb@gmail.com>
2023-08-25 13:16:27 +0500

    contact-form

---
+++ js/app.js
@@ -0,0 +1,7 @@
+function submitData(){
+    console.log("Fullname >>> ",document.getElementById("fullname").value);
+    console.log("Email >>> ",document.getElementById("email").value);
+    console.log("Phone >>> ",document.getElementById("phone").value);
+    console.log("Profession >>> ",document.getElementById("profession").value);
+    console.log("Message >>> ",document.getElementById("message").value);
+}
